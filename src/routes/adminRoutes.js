de5b76014509f370463437c1fad87bd5946dcbac
abhinivesh3 <abhinivesh.rcse2020@sece.ac.in>
2023-06-29 23:39:18 +0530

    commit

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -6,13 +6,29 @@ const adminController = require('../controllers/adminController');
 router.get('/',(req,res)=>{
     res.render('adminLogin')
 })
+
+//get admin Login Page
 router.get('/login', adminController.getAdminLoginPage);
+
+//post create login
 router.post('/login', adminController.login);
+
+//get  logout
 router.get('/logout', adminController.logout);
+
+//get Dashboard Page
 router.get('/dashboard', adminController.getDashboardPage);
+
+//get Search Page to admin main page
 router.get('/search', adminController.getSearchPage);
+
+//post create for Centre
 router.post('/centres', adminController.addCentre);
+
+//get Dosage Details
 router.get('/dosage', adminController.getDosageDetails);
+
+//post create for remove centre
 router.post('/remove-centre', adminController.removeCentre);
 
 module.exports = router;
